2a69186941999bf1c69014e363b97205c121beff
JAVA-LK <2726028068@qq.com>
2022-07-27 10:52:21 +0800

    二奢商城完全体基础框架搭建。新增商品服务复杂mapper查询、FastDFS搭建（文件存储微服务功能）、搭建网关微服务 第一次提交的bug已解决，此次bug见附录文章

--- ydles_parent/ydles_service/ydles_service_file/src/main/java/com/ydles/file/util/FastDFSFile.java
+++ ydles_parent/ydles_service/ydles_service_file/src/main/java/com/ydles/file/util/FastDFSFile.java
@@ -1,5 +1,12 @@
 package com.ydles.file.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FastDFSFile {
     //文件名字
     private String name;
@@ -28,43 +35,5 @@ public class FastDFSFile {
         this.ext = ext;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public byte[] getContent() {
-        return content;
-    }
-
-    public void setContent(byte[] content) {
-        this.content = content;
-    }
-
-    public String getExt() {
-        return ext;
-    }
-
-    public void setExt(String ext) {
-        this.ext = ext;
-    }
-
-    public String getMd5() {
-        return md5;
-    }
-
-    public void setMd5(String md5) {
-        this.md5 = md5;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
